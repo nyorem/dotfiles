ac65eec0a2286dac6fec21794e634bc69bace213
Jocelyn MEYRON <Jocelyn.Meyron@grenoble-inp.org>
2017-02-23 22:52:48 +0100

    Update rikaichan for vimperator

--- firefox/.vimperator/plugin/rikai.js
+++ firefox/.vimperator/plugin/rikai.js
@@ -1,10 +1,23 @@
 /**
 * Toggle Rikaichan
-* source: http://why-would-you-ever-want-a-short-url.blogspot.com/2016/02/vimperator-rikaichan.html
+* source:
+* - for firefox < 51: http://why-would-you-ever-want-a-short-url.blogspot.com/2016/02/vimperator-rikaichan.html
+* - for firefox >= 51: https://developer.mozilla.org/en-US/docs/Tools/Browser_Toolbox
 */
 
 commands.addUserCommand(["rikai[chan]","rk"], "Toggle Rikaichan",
                         function() {
-                            rcxMain.toggle();
+                            // For Firefox < 51
+                            // rcxMain.toggle();
+
+                            // For Firefox >= 51
+                            // extracted from the onToggleButton(ev) function
+                            let browser = rcxMain.getBrowser();
+                            let on = ((typeof(browser._rikaichan_) != 'undefined') && (browser._rikaichan_.enabled));
+                            if (on) {
+                                rcxMain.disableOnBrowser(browser);
+                            } else {
+                                rcxMain.enableOnBrowser(browser);
+                            }
                         });
 
