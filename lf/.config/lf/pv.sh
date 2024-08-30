d5bbb7fb31a699e6d3361f38c63421e749708685
Meyron, Jocelyn <JocelynMeyron@eaton.com>
2024-08-30 18:17:34 +0200

    fix: make pv more robust and add tig commit ammend shortcut

--- lf/.config/lf/pv.sh
+++ lf/.config/lf/pv.sh
@@ -2,11 +2,20 @@
 
 unset COLORTERM
 
+function my_cat()
+{
+    if [ -x "$(command -v bat)" ]; then
+        /usr/bin/batcat --color always "$@"
+    else
+        cat "$@"
+    fi
+}
+
 case "$1" in
     *.tar*) tar tf "$1";;
     *.zip) unzip -l "$1";;
     # *.rar) unrar l "$1";;
     # *.7z) 7z l "$1";;
     # *.pdf) pdftotext "$1" -;;
-    *) /usr/bin/batcat --color always "$@" ;;
+    *) my_cat "$@" ;;
 esac
